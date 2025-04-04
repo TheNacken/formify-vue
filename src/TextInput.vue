5c0ffba2d8b74f1940926f8ee6a858d5b78582e7
TheNacken <justus.nacken@gmail.com>
2025-04-04 17:44:16 +0200

    feat: enhance TextInput component with ref support and improved textarea styling

--- src/TextInput.vue
+++ src/TextInput.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import InputLabel from '@/InputLabel.vue'
+import { InputLabel } from './index'
+import { ref } from 'vue'
 
 defineProps({
   inputId: {
@@ -9,17 +10,25 @@ defineProps({
 })
 
 const model = defineModel<string>({ default: '' })
+const inputRef = ref<HTMLTextAreaElement | null>(null)
 </script>
 
 <template>
   <div
-    class="relative m-2 h-fit cursor-text rounded-md border-2 border-black/50 p-2 focus-within:border-blue-900"
+    class="relative m-2 cursor-text rounded-md border-2 border-black/50 p-2 focus-within:border-blue-900"
     style="background-color: inherit"
+    @click="inputRef?.focus()"
   >
     <InputLabel :inputId="inputId">
       <slot />
     </InputLabel>
-    <textarea v-model="model" :id="inputId" style="resize: none" class="outline-none"></textarea>
+    <textarea
+      v-model="model"
+      :id="inputId"
+      style="resize: none"
+      class="w-full outline-none"
+      ref="inputRef"
+    />
   </div>
 </template>
 

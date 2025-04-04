1394de7f22431e453b30540eaa276fea251322b5
TheNacken <you@example.com>
2025-04-04 13:44:05 +0200

    feat: enhance InputLabel and NumberInput components for dynamic background color and prefix support

--- src/InputLabel.vue
+++ src/InputLabel.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
-defineProps({
+import { onMounted, ref } from 'vue'
+
+const props = defineProps({
   inputId: String,
+})
+
+const backgroundColor = ref('white')
+
+onMounted(() => {
+  let element = document.getElementById(props.inputId)?.parentElement
+
+  while (element) {
+    const bgColor = getComputedStyle(element).backgroundColor
+    if (bgColor !== 'rgba(0, 0, 0, 0)' && bgColor !== 'transparent') {
+      backgroundColor.value = bgColor
+      break
+    }
+    element = element.parentElement
+  }
 })
 </script>
 
 <template>
   <label
     :for="inputId"
-    class="absolute -top-3 left-2 cursor-text bg-white px-1 text-sm text-nowrap text-gray-500"
+    :style="{ backgroundColor }"
+    class="absolute -top-3 left-2 cursor-text px-1 text-sm text-nowrap text-gray-500"
   >
     <slot />
   </label>

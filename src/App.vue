2e575b677bbf25917c92e0082a091fbf179f3eb2
TheNacken <justus.nacken@gmail.com>
2025-04-04 17:45:15 +0200

    feat: enhance App.vue with additional components and improved Select options

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { NumberInput, Checkbox, MultipleSelect, Slider } from './index'
-import TextInput from '@/TextInput.vue'
+import { NumberInput, Checkbox, MultipleSelect, Select, Slider, TextInput, Button } from './index'
 </script>
 
 <template>
@@ -18,7 +17,16 @@ import TextInput from '@/TextInput.vue'
   </div>
   <Checkbox name="checkbox-input" value="test" input-id="checkbox-input" size="md"></Checkbox>
   <MultipleSelect input-id="multiple-select-input">Test</MultipleSelect>
-  <Select :options="[{ name: 'test', value: 'test' }]" input-id="select-input"></Select>
+  <Select
+    :options="[
+      { name: 'test', value: 'test' },
+      { name: 'Testsetsetsetsteste', value: 'atdzatdwtuzdtatzdtaz' },
+    ]"
+    input-id="select-input"
+    >Test</Select
+  >
   <Slider></Slider>
-  <TextInput input-id="text-input"></TextInput>
+  <Slider :max="100" :min="0" :step="1" :markers="[0, 25, 50, 75, 100]"></Slider>
+  <TextInput class="h-50" input-id="text-input"></TextInput>
+  <Button label="Test" @click="console.log('test')"></Button>
 </template>
